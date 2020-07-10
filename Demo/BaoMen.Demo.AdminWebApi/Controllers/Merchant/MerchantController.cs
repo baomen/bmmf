fb8672a6c242ebe4a130cb10f0283a559bd89c92
draweye <draweye@sohu.com>
2020-07-10 17:25:50 +0800

    修改版本号

---
+++ Demo/BaoMen.Demo.AdminWebApi/Controllers/Merchant/MerchantController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BaoMen.MultiMerchant.Merchant.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaoMen.Demo.AdminWebApi.Merchant.Controllers
+{
+    /// <summary>
+    /// 商户
+    /// </summary>
+    [Route("api/merchant/[controller]/[action]")]
+    public class MerchantController : MultiMerchant.Web.Merchant.Controllers.MerchantController
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="manager">业务逻辑实例</param>
+        /// <param name="mapper">AutoMapper实例</param>
+        public MerchantController(IMerchantManager manager, IMapper mapper) : base(manager, mapper)
+        {
+
+        }
+    }
+}
