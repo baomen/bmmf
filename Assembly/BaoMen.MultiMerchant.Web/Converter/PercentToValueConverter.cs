2806b98fa5428ff0ee07277a39a77eb3c4c73096
draweye <draweye@sohu.com>
2020-12-30 12:40:17 +0800

    去掉LogicOperator

---
+++ Assembly/BaoMen.MultiMerchant.Web/Converter/PercentToValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+
+namespace BaoMen.MultiMerchant.Web.Converter
+{
+    /// <summary>
+    /// 百分比 --> 值
+    /// </summary>
+    public class PercentToValueConverter : IValueConverter<decimal, decimal>
+    {
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <param name="sourceMember">源</param>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public decimal Convert(decimal sourceMember, ResolutionContext context)
+        {
+            return sourceMember / 100;
+        }
+    }
+}
---
+++ Assembly/BaoMen.MultiMerchant.Web/Converter/ValueToPercentConverter.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+
+namespace BaoMen.MultiMerchant.Web.Converter
+{
+    /// <summary>
+    /// 值 --> 百分比
+    /// </summary>
+    public class ValueToPercentConverter : IValueConverter<decimal, decimal>
+    {
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <param name="sourceMember">源</param>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public decimal Convert(decimal sourceMember, ResolutionContext context)
+        {
+            return sourceMember * 100;
+        }
+    }
+}
