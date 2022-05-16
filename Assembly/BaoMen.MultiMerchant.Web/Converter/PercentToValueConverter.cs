57b8487c5d09e963e6f9bae98257e08152bdee62
draweye <draweye@sohu.com>
2022-05-16 10:09:28 +0800

    增加百分比转换的类

--- Assembly/BaoMen.MultiMerchant.Web/Converter/PercentToValueConverter.cs
+++ Assembly/BaoMen.MultiMerchant.Web/Converter/PercentToValueConverter.cs
@@ -6,7 +6,7 @@ namespace BaoMen.MultiMerchant.Web.Converter
     /// <summary>
     /// 百分比 --> 值
     /// </summary>
-    public class PercentToValueConverter : IValueConverter<decimal, decimal>
+    public class PercentToValueConverter : IValueConverter<decimal, decimal>,IValueConverter<double, double>
     {
         /// <summary>
         /// 转换
@@ -16,7 +16,18 @@ namespace BaoMen.MultiMerchant.Web.Converter
         /// <returns></returns>
         public decimal Convert(decimal sourceMember, ResolutionContext context)
         {
-            return sourceMember / 100;
+            return (decimal)(double)(sourceMember / 100);
+        }
+
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <param name="sourceMember">源</param>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public double Convert(double sourceMember, ResolutionContext context)
+        {
+            return (double)(sourceMember / 100);
         }
     }
 }
--- Assembly/BaoMen.MultiMerchant.Web/Converter/ValueToPercentConverter.cs
+++ Assembly/BaoMen.MultiMerchant.Web/Converter/ValueToPercentConverter.cs
@@ -6,7 +6,7 @@ namespace BaoMen.MultiMerchant.Web.Converter
     /// <summary>
     /// 值 --> 百分比
     /// </summary>
-    public class ValueToPercentConverter : IValueConverter<decimal, decimal>
+    public class ValueToPercentConverter : IValueConverter<decimal, decimal>, IValueConverter<double, double>
     {
         /// <summary>
         /// 转换
@@ -15,6 +15,17 @@ namespace BaoMen.MultiMerchant.Web.Converter
         /// <param name="context">上下文</param>
         /// <returns></returns>
         public decimal Convert(decimal sourceMember, ResolutionContext context)
+        {
+            return (decimal)(double)(sourceMember * 100);
+        }
+
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <param name="sourceMember">源</param>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public double Convert(double sourceMember, ResolutionContext context)
         {
             return sourceMember * 100;
         }
